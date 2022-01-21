ea5447c747f362a0fecf2c5ae03f65ebb013bda1
EmilyT <emilython@hotmail.com>
2022-01-21 16:34:05 -0600

    Added resume page and styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import ContactForm from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Resume></Resume>
         <ContactForm></ContactForm>
         <About></About>
       </main>
---
+++ src/components/Resume/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <section className="resume--container">
+      <h1>Resume</h1>
+      <div className="skill--list">
+        <h2>I am experienced in the following technologies:</h2>
+        <ul>
+          <li>HTML and CSS</li>
+          <li>Javascript and JQuery</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>React</li>
+          <li>SQL and Sequelize</li>
+          <li>MongoDB and Mongoose</li>
+        </ul>
+      </div>
+      <p>
+        To check out my full resume, check out my linkedIn page{' '}
+        <a href="https://www.linkedin.com/in/emmykay/">here</a>
+      </p>
+    </section>
+  );
+}
+
+export default Resume;
