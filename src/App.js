2a8663b87ed3131c93d979adb0596b1419ee610a
EmilyT <emilython@hotmail.com>
2022-01-21 20:00:07 -0600

    added portfolio page and styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from './components/About';
 import Nav from './components/Nav';
 import ContactForm from './components/Contact';
 import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Portfolio></Portfolio>
         <Resume></Resume>
         <ContactForm></ContactForm>
         <About></About>
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import kanye from '../../assets/images/kanye.jpg';
+import dog from '../../assets/images/dog.jpg';
+import profile from '../../assets/images/profile.jpg';
+import run from '../../assets/images/run.jpg';
+import quiz from '../../assets/images/quiz.jpg';
+import notes from '../../assets/images/notes.jpg';
+
+function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h1>Portfolio</h1>
+      <div className="flex-row">
+        <div className="img--container">
+          <img src={kanye} className="img-thumbnail mx-1"></img>
+          <span>
+            <a href="https://concord511.github.io/Quote-Battle/">
+              Kanye Guess - HTML, CSS, Javascript
+            </a>
+            <br></br>
+            <a href="https://github.com/Concord511/Quote-Battle">Github</a>
+          </span>
+        </div>
+        <div className="img--container">
+          <img src={dog} className="img-thumbnail mx-1"></img>
+          <span>
+            <a href="https://secret-sea-39551.herokuapp.com/">
+              Rate My Dog - SQL, Sequelize
+            </a>
+            <br></br>
+            <a href="https://github.com/cassie-s/the-dog-blog">Github</a>
+          </span>
+        </div>
+        <div className="img--container">
+          <img src={profile} className="img-thumbnail mx-1"></img>
+          <span>
+            <a href="https://github.com/EmilyThatsMe/profile-generator">
+              Profile generator - Node, Express, Inquirer
+            </a>
+            <br></br>
+            <a href="https://github.com/EmilyThatsMe/profile-generator">
+              Github
+            </a>
+          </span>
+        </div>
+        <div className="img--container">
+          <img src={quiz} className="img-thumbnail mx-1"></img>
+          <span>
+            <a href="https://emilythatsme.github.io/code-quiz/">
+              Code Quiz - CSS, Javascript, Moment.js
+            </a>
+            <br></br>
+            <a href="https://github.com/EmilyThatsMe/code-quiz">Github</a>
+          </span>
+        </div>
+        <div className="img--container">
+          <img src={run} className="img-thumbnail mx-1"></img>
+          <span>
+            <a href="https://emilythatsme.github.io/run-buddy/">
+              Run Buddy - HTML, CSS
+            </a>
+            <br></br>
+            <a href="https://github.com/EmilyThatsMe/run-buddy">Github</a>
+          </span>
+        </div>
+        <div className="img--container">
+          <img src={notes} className="img-thumbnail mx-1"></img>
+          <span>
+            <a href="https://emilythatsme.github.io/express-note-taker/">
+              Note Taker - Node, Express
+            </a>
+            <br></br>
+            <a href="https://github.com/EmilyThatsMe/express-note-taker">
+              Github
+            </a>
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Portfolio;
