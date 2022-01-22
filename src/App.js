5309804a3f526e4b76f0f573db665733ff25ec0e
EmilyT <emilython@hotmail.com>
2022-01-21 20:50:39 -0600

    added footer links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Nav from './components/Nav';
 import ContactForm from './components/Contact';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
+import Github from './assets/images/iconmonstr-github-1.svg';
+import Linkedin from './assets/images/iconmonstr-linkedin-3.svg';
+import Stack from './assets/images/iconmonstr-stackoverflow-1.svg';
 
 function App() {
   return (
@@ -16,7 +19,20 @@ function App() {
         <ContactForm></ContactForm>
         <About></About>
       </main>
-      <footer>Emily Thon</footer>
+      <footer>
+        <a href="https://github.com/EmilyThatsMe">
+          {' '}
+          <img src={Github}></img>
+        </a>
+        <a href="https://www.linkedin.com/in/emmykay/">
+          {' '}
+          <img src={Linkedin}></img>
+        </a>
+        <a href="#">
+          {' '}
+          <img src={Stack}></img>
+        </a>
+      </footer>
     </div>
   );
 }
